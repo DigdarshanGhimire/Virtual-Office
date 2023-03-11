92c94212f76ed1d404820621e1f777322ce8c40b
Digdarshan_Ghimire <lambahidman701@gmail.com>
2023-03-11 13:39:12 +0545

    Home layout(Done)

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
-if (window.top !== window.self) {
-    window.top.location = window.self.location;
+
+let hamburger = document.getElementById('hamburg');
+let menu = document.getElementById('navlist')
+let open = false;
+
+function openclosemenu(){
+    if (open){
+        menu.style.display = 'none';
+        open=false;
+    }
+    else{
+        menu.style.display = 'flex';
+        open=true;
+    }
 }
+
+hamburger.addEventListener('click',openclosemenu)
