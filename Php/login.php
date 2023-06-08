75998b3fbb0fbc5d37bc2f34360e1c590a485674
Great Erivona Fejiro <126071205+gerivona@users.noreply.github.com>
2023-06-08 21:24:00 +0100

    Update login.php

--- Php/login.php
+++ Php/login.php
@@ -6,9 +6,9 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-Frame-Options" content="DENY">
-        <meta http-equiv="Content-Security-Policy" content="script-src 'self' Virtual-Office.com;">
+                <meta http-equiv="Content-Security-Policy" content="script-src 'self' Virtual-Office.com;">
 		<meta charset="utf-8">
-	</head>
+</head>
 	<body>
 		<center><h1>Zenith Virtual Office</h1></center>
 		<nav>
