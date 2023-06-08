b077fd9ae3195bf12a9c1270de800ff2e481abe0
Great Erivona Fejiro <126071205+gerivona@users.noreply.github.com>
2023-06-08 21:19:34 +0100

    Update Welcome.php

--- Php/Welcome.php
+++ Php/Welcome.php
@@ -1,8 +1,15 @@
-<html>
-<body>
-
-Welcome <?php echo $_GET["name"]; ?><br>
-Your email address is: <?php echo $_GET["email"]; ?>
-
-</body>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-Frame-Options" content="DENY">
+    <meta http-equiv="Content-Security-Policy" content="script-src 'self' Virtual-Office.com;">
+    <title> Home </title>
+    <link rel="stylesheet" href="welcome.css">
+    </head>
+    <body>
+            <p> Welcome <?php echo $_GET["name"]; ?> </p>
+    </body>
 </html>
