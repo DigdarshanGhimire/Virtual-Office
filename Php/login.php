d7e13d399fe03c7161de1180b96e0385fc05224b
Great Erivona Fejiro <126071205+gerivona@users.noreply.github.com>
2023-06-08 21:23:08 +0100

    Update login.php

--- Php/login.php
+++ Php/login.php
@@ -1,1 +1,35 @@
-
+<Doctype html!>
+<head>
+  <html lang="en">
+		<title>Login</title>
+		<link rel="stylesheet" type="text/css" href="Login.css">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-Frame-Options" content="DENY">
+        <meta http-equiv="Content-Security-Policy" content="script-src 'self' Virtual-Office.com;">
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<center><h1>Zenith Virtual Office</h1></center>
+		<nav>
+			<li><a href="Home.html">Home</a></li>
+			<li><a href="#">About us</a></li>
+			<li><a href="#">Contact us </a></li>
+		</nav>
+		<br>
+         	<div class="loginbox">
+			<h1>Login Here</h1>
+			<form>
+				<center>
+			     	<p>Username</p>
+				    <input type="text" placeholder="Enter your Username">
+			    	<p>Password</p>
+				    <input type="password" placeholder="Enter your password">
+				    <input type="submit" placeholder="Login">
+				    <a href="Forgot_Password.html">Forgotten your password?</a>
+				    <a href="Register.html">Create an account</a>
+			    </center>
+			</form>
+		</div>
+	</body>
+</html>
